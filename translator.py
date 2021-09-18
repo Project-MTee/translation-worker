2ca89e76fb3802ea3e8b833b3a6c09c3c12e900e
maalitars <tarsmaali@gmail.com>
2021-09-18 12:43:06 +0300

    Enhanced Translator class, implementing pretrained model loading and translation methods.

--- translator.py
+++ translator.py
@@ -1,14 +1,20 @@
 import logging
 from typing import List
+from fairseq.models.transformer import TransformerModel
+
 
 logger = logging.getLogger("nmt-worker")
 
 
 class Translator:
-    def __init__(self):
-        # TODO: load model
-        pass
+    def __init__(self, checkpoint_path, dict_path, spm_model_path):
+        self.model = TransformerModel.from_pretrained(
+            checkpoint_path,
+            checkpoint_file='checkpoint_best.pt',
+            data_name_or_path=dict_path,
+            bpe='sentencepiece',
+            sentencepiece_model=spm_model_path
+        )
 
     def translate(self, sentences: List[str], src: str, tgt: str, domain: str, input_type: str = "plain") -> List[str]:
-        # TODO
-        pass
+        return self.model.translate(sentences)
