6ffb682c53b4003026d01b3569220e31649172e8
Liisa <liisaratsep@gmail.com>
2021-09-21 09:11:32 +0300

    improved tag processing

--- tag_utils.py
+++ tag_utils.py
@@ -3,28 +3,88 @@ Fallback preprocessing method to meet the bare minimum of tagged translation con
 All tags are removed and appended to the translation.
 """
 import re
-from typing import List
+import html
+from typing import List, Tuple
 
 tag_patterns = {
-    'document': r'</?(?:x|g|bx|ex)[^>]+>',
+    'document': r'</?(?:x|g|bx|ex)[^>]*>',
     'web': r'</?(?:a|abbr|acronym|em|strong|b|i|s|strike|u|span|del|ins|sub|sup|code|samp|kbd|var|small|mark|ruby|rt'
-           r'|rp|bdi|bdo)[0-9]+>'
+           r'|rp|bdi|bdo)[0-9]+/?>',
+    # both formats for testing mixed test files:
+    'test': r'</?(?:x|g|bx|ex)[^>]*>|'
+           r'</?(?:a|abbr|acronym|em|strong|b|i|s|strike|u|span|del|ins|sub|sup|code|samp|kbd|var|small|mark|ruby|rt'
+           r'|rp|bdi|bdo)[0-9]+/?>'
 }
 
+bpt = r'<[^/][^>]*>'
+ept = r'</[^>]*>'
 
-def _preprocess_tags(sentences: List[str], input_type: str) -> (List[str], List[str]):
+# Other symbols do not need replacing
+html_entities = {'<': '&lt;',
+                 '>': '&gt;',
+                 '&': '&amp;'}
+
+
+def preprocess_tags(sentences: List[str], input_type: str) -> (List[str], List[List[Tuple[str, int, str]]]):
     if input_type in tag_patterns:
         pattern = tag_patterns[input_type]
-        tags = [''.join(re.findall(pattern, sentence)) for sentence in sentences]
-        sentences = [re.sub(pattern, '', sentence) for sentence in sentences]
-        sentences = [re.sub(r'(\s)+', r'\1', sentence).strip() for sentence in sentences]
+        clean_sentences = []
+        tags = []
+        for sentence in sentences:
+            sentence = sentence.strip()
+            sentence_tags = []  # list of tuples (tag, indexes, tag_type)
+
+            tokens = list(filter(None, re.split(rf' |{pattern}', sentence)))
+            tokens_w_tags = list(filter(None, re.split(rf' |({pattern})', sentence)))
+
+            clean_sentences.append(' '.join(tokens).strip())
+
+            for idx, item in enumerate(tokens_w_tags):
+                idx = idx - len(sentence_tags)
+                if len(tokens) <= idx or item != tokens[idx]:
+                    if len(tokens) <= idx:
+                        idx = -1
+
+                    if re.match(bpt, item):
+                        sentence_tags.append((item, idx, 'bpt'))
+                    elif re.match(ept, item):
+                        sentence_tags.append((item, idx, 'ept'))
+                    else:
+                        sentence_tags.append((item, idx, 'ph'))
+
+            tags.append(sentence_tags)
+
     else:
-        tags = ['' for _ in sentences]
+        clean_sentences = sentences
+        tags = [[] for _ in sentences]
 
-    return sentences, tags
+    clean_sentences = [html.unescape(sentence) for sentence in clean_sentences]
+
+    return clean_sentences, tags
 
 
-def _postprocess_tags(translations: List[str], tags: List[str]):
-    translations = [f'{translation}{tag}' for tag, translation in zip(translations, tags)]
+def postprocess_tags(translations: List[str], tags: List[List[Tuple[str, int, str]]]):
+    for symbol, entity in html_entities.items():
+        translations = [sentence.replace(symbol, entity) for sentence in translations]
 
-    return translations
+    retagged = []
+
+    for translation, sentence_tags in zip(translations, tags):
+        retagged_sentence = []
+
+        tokens = translation.split(' ')
+
+        for idx, token in enumerate(tokens):
+            whitespace_added = False
+            while sentence_tags and sentence_tags[0][1] == idx:
+                if not whitespace_added and sentence_tags[0][2] == 'bpt':
+                    retagged_sentence.append(' ')
+                    whitespace_added = True
+                retagged_sentence.append(sentence_tags.pop(0)[0])
+            if not whitespace_added:
+                retagged_sentence.append(' ')
+            retagged_sentence.append(token)
+
+        retagged.append((''.join(retagged_sentence) + ''.join([tag for tag, _, _ in sentence_tags])).strip())
+
+    return retagged
--- translator.py
+++ translator.py
@@ -2,6 +2,7 @@ import logging
 from typing import List
 from fairseq.models.transformer import TransformerModel
 
+from tag_utils import preprocess_tags, postprocess_tags
 
 logger = logging.getLogger("nmt-worker")
 
@@ -16,4 +17,38 @@ class Translator:
         )
 
     def translate(self, sentences: List[str], src: str, tgt: str, domain: str, input_type: str = "plain") -> List[str]:
-        return self.model.translate(sentences)
+        detagged, tags = preprocess_tags(sentences, input_type)
+
+        # Translations of empty source sentences are deleted, because NMT may hallucinate
+        translations = [translation if detagged[idx] != '' else '' for idx, translation in
+                        enumerate(self.model.translate(detagged))]
+
+        retagged = postprocess_tags(translations, tags)
+
+        return retagged
+
+
+if __name__ == "__main__":
+    # File translation mode
+    from argparse import ArgumentParser, FileType
+
+    parser = ArgumentParser()
+    parser.add_argument('checkpoint', help="checkpoint folder path")
+    parser.add_argument('spm', type=FileType('r'), help="sentencepiece .model file path")
+    parser.add_argument('input', type=FileType('r'), help="input text file")
+    parser.add_argument('output', type=FileType('w'), help="output text file")
+    args = parser.parse_args()
+
+    translator = Translator(args.checkpoint, args.spm.name)
+
+    in_file = open(args.input.name, 'r', encoding='utf-8')
+    out_file = open(args.output.name, 'w', encoding='utf-8')
+
+    while True:
+        text = in_file.readline()
+        if not text:
+            break
+        out_file.write(translator.translate([text], 'en', 'et', 'general', 'test')[0]+'\n')
+
+    in_file.close()
+    out_file.close()
